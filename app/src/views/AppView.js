04965b14b19a30e932e8ed2eff3db1af16ec6eb8
mikefreeman <michael.p.freeman@gmail.com>
2014-06-17 16:55:01 -0700

    Refactored header bar creation and added icon and year/month surfaces

--- app/src/views/AppView.js
+++ app/src/views/AppView.js
@@ -4,43 +4,52 @@ define(function(require, exports, module) {
   var Surface = require('famous/core/Surface');
   var Modifier = require('famous/core/Modifier');
   var Transform = require('famous/core/Transform');
+  var RenderNode = require('famous/core/RenderNode');
   var HeaderFooterLayout = require('famous/views/HeaderFooterLayout');
   var GridLayout = require("famous/views/GridLayout");
   var Transitionable = require('famous/transitions/Transitionable');
+  var ImageSurface = require('famous/surfaces/ImageSurface');
 
   function AppView() {
     View.apply(this, arguments);
 
     _createLayout.call(this);
-    _createDaysOfWeekBar.call(this);
+    _createHeader.call(this);
   }
 
   AppView.prototype = Object.create(View.prototype);
   AppView.prototype.constructor = AppView;
 
   AppView.DEFAULT_OPTIONS = {
+    headerSize: 60
   };
 
   function _createLayout() {
     this.layout = new HeaderFooterLayout({
-      headerSize: 60
+      headerSize: this.options.headerSize
     });
 
-    this.headerModifier = new Modifier({});
+    var layoutModifier = new Modifier({
+      transform: Transform.translate(0, 0, 0.1)
+    });
 
-    var header = new Surface({
-      size: [undefined, undefined],
+    this.add(layoutModifier).add(this.layout);
+  }
+
+  function _createHeader() {
+    // background surface
+    var backgroundSurface = new Surface({
       properties: {
         backgroundColor: '#F2F2F2'
       }
     });
 
-    this.layout.header.add(this.headerModifier).add(header);
-    this.add(this.layout);
-  }
+    var backgroundModifier = new Modifier({
+      transform: Transform.behind
+    });
 
-  function _createDaysOfWeekBar() {
-    // create static days of the week bar
+    
+    // static days of the week bar
     var fontColor;
     var letterSurface;
     var letterSurfaces = [];
@@ -74,6 +83,58 @@ define(function(require, exports, module) {
       transform: Transform.translate(0, 48, 5)
     });
 
+    // back icon
+    var backIcon = new ImageSurface({
+      size: [22, 22],
+      content:'content/images/back_icon.png'
+    });
+
+    var backIconModifier = new Modifier({
+      align: [0.06, 0.4],
+      origin: [0.5, 0.5]
+    });
+
+    // year and month surfaces
+    var yearSurface = new Surface({
+      content: '2014',
+      properties: {
+        color: 'red',
+        textAlign: 'center',
+        lineHeight: '50px',
+        fontSize: '14px',
+        fontFamily: 'sans-serif',
+      }
+    });
+
+    var monthSurface = new Surface({
+      content: 'June',
+      properties: {
+        color: 'red',
+        textAlign: 'right',
+        lineHeight: '50px',
+        fontSize: '14px',
+        fontFamily: 'sans-serif'
+      }
+    });
+
+    this.yearModifier = new Modifier({
+      align: [0.15, 0.5],
+      origin: [0.5, 0.5],
+      opacity: 0.999,
+      transform: Transform.translate(0, 0, 3)
+    });
+
+    this.monthModifier = new Modifier({
+      align: [0.5, 0.5],
+      origin: [0.5, 0.5],
+      opacity: 0.001,
+      transform: Transform.translate(0, 0, 3)
+    });
+
+    this.layout.header.add(backgroundModifier).add(backgroundSurface);
+    this.layout.header.add(this.yearModifier).add(yearSurface);
+    this.layout.header.add(this.monthModifier).add(monthSurface);
+    this.layout.header.add(backIconModifier).add(backIcon);
     this.add(letterGridModifier).add(letterGrid);
   }
 
