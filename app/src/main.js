1307d52823a70cd0bcb644940e88a547df1352c4
mikefreeman <michael.p.freeman@gmail.com>
2014-06-17 13:44:57 -0700

    Removed unused code and created an AppView

--- app/src/main.js
+++ app/src/main.js
@@ -3,27 +3,10 @@ define(function(require, exports, module) {
     'use strict';
     // import dependencies
     var Engine = require('famous/core/Engine');
-    var Modifier = require('famous/core/Modifier');
-    var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
+    var AppView = require('views/AppView');
 
-    // create the main context
     var mainContext = Engine.createContext();
+    var appView = new AppView();
 
-    // your app here
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: '/content/images/famous_logo.png',
-        classes: ['backfaceVisibility']
-    });
-
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform : function() {
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
-        }
-    });
-
-    mainContext.add(centerSpinModifier).add(logo);
+    mainContext.add(appView);
 });
