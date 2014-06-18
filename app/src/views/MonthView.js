24785e593895cc15f58e20d76dcf0180b8b7a0ae
mikefreeman <michael.p.freeman@gmail.com>
2014-06-18 08:48:20 -0700

    Added surface for month name

--- app/src/views/MonthView.js
+++ app/src/views/MonthView.js
@@ -5,9 +5,13 @@ define(function(require, exports, module) {
   var Modifier = require('famous/core/Modifier');
   var Transform = require('famous/core/Transform');
   var Transitionable = require('famous/transitions/Transitionable');
+  var StateModifier     = require('famous/modifiers/StateModifier');
 
   function MonthView() {
     View.apply(this, arguments);
+    this.mods = [];
+
+    _createMonthName.call(this);
   }
 
   MonthView.prototype = Object.create(View.prototype);
@@ -15,6 +19,25 @@ define(function(require, exports, module) {
 
   MonthView.DEFAULT_OPTIONS = {
   };
+
+  function _createMonthName() {
+    var monthName = new Surface({
+      size: [undefined, 60],
+      content: '&nbsp;&nbsp;JUN',
+      properties: {
+        lineHeight: '80px',
+        fontFamily: 'sans-serif',
+        fontSize: '14px',
+        color: 'red',
+        pointerEvents: 'none'
+      }
+    });
+
+    var slideMod = new StateModifier();
+    this.mods.push(slideMod);
+
+    this.add(slideMod).add(monthName);
+  }
 
   module.exports = MonthView;
 });
