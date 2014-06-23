dc1970971ae8c8597f54f64e4e26eafa02b01b9a
Alex West <alexanderwwest@gmail.com>
2014-06-23 14:16:13 -0700

    Updated EventsView with data storage functions

--- app/src/views/EventsView.js
+++ app/src/views/EventsView.js
@@ -8,12 +8,57 @@ define(function(require, exports, module) {
   
   function EventsView() {
     View.apply(this, arguments);
+
+
   }
+
+  function supports_html5_storage() {
+    try {
+      return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  function saveEvent(newEvent) {
+
+    //Retrieve events for date from local storage
+    var events = window.localStorage.getItem(eventDate);
+
+    //Check if any events exist
+    if(events === null){
+      //Initialize events to array
+      events = [];
+    }else{
+      //Parse events array from string
+      events = JSON.parse(events);
+    }
+
+    //create new event object from input - TBD in Calling Function
+
+    //Object schema:
+    //{date: eventToBeSaved.date, 
+      // start: eventToBeSaved.start, 
+      // end: eventEndTime, 
+      // title: eventTitle, 
+      // description: eventBody};
+    
+    //add new event to events array
+    events.push(newEvent);
+    //Store updated events array in local storage
+    window.localStorage.setItem(eventDate, JSON.stringify(events));
+  }
+
+  function getEvents(date){
+    //returns events array for given date
+    return JSON.parse(window.localStorage.getItem(date));
+  }
+
+  EventsView.DEFAULT_OPTIONS = {};
   
   EventsView.prototype = Object.create(View.prototype);
   EventsView.prototype.constructor = EventsView;
   
-  EventsView.DEFAULT_OPTIONS = {};
   
   module.exports = EventsView;
 });
