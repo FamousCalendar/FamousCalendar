2e9421c0884038e4fb21f0d9ca5f9d9bdda57d49
mikefreeman <michael.p.freeman@gmail.com>
2014-06-24 10:59:08 -0700

    Added getter/setter methods and refactored to be more dynamic

--- app/src/views/DayBoxView.js
+++ app/src/views/DayBoxView.js
@@ -20,38 +20,69 @@ define(function(require, exports, module) {
   DayBoxView.prototype = Object.create(View.prototype);
   DayBoxView.prototype.constructor = DayBoxView;
 
+  DayBoxView.prototype.isWeekend = function() {
+    return this.options.weekDay === 0 || this.options.weekDay === 6;
+  };
+
+  DayBoxView.prototype.setDate = function(year, month, day, week) {
+    this.numberSurface.setContent(day);
+    // need to check if an event exists for this date here
+    this.options.day = day;
+    this.options.month = month;
+    this.options.year = year;
+    this.options.week = week;
+    return true;
+  };
+
+  DayBoxView.prototype.getDate = function() {
+    return { 
+      day: this.options.day, 
+      month: this.options.month,
+      year: this.options.year,
+      week: this.options.week,
+      weekDay: this.options.weekDay
+    };
+  };
+
   DayBoxView.DEFAULT_OPTIONS = {
-    number: 1,
-    fontColor: 'black',
-    // dayName-month-day-year-weekofmonth
-    id: 'Wednesday-January-1-2014-1'
+    day: 1,
+    dayName: 'Sunday',
+    week: 1,
+    weekDay: 0,
+    month: 1,
+    year: 2014,
+    weekdayColor: 'black',
+    weekendColor: 'grey',
+    backgroundColor: 'white',
+    borderColor: 'lightgrey',
+    selectedColor: 'black',
+    font: 'sans-serif'
   };
 
   function _createBackground() {
-    var borderColor = (this.options.number < 0) ? '' : 'lightgrey';
+    var borderColor = this.options.day ? this.options.borderColor : this.options.backgroundColor;
 
     this.backgroundSurface = new Surface({
       size: [undefined, undefined],
       properties: {
         borderTop: '1px solid ' + borderColor,
-        backgroundColor: 'white',
-        id: this.options.id
+        backgroundColor: this.options.backgroundColor,
+        zIndex: 1
       }
     });
 
-    this.backgroundModifier = new Modifier({});
+    this.backgroundModifier = new Modifier();
 
     this.backgroundSurface.on('click', function(data) {
       this.numberSurface.setProperties({
         color: 'white',
         fontWeight: 'bold',
         fontSize: '18px',
-        backgroundColor: 'black',
-        borderRadius: '99999px'
+        backgroundColor: this.options.selectedColor,
+        borderRadius: '50px'
       });
 
-      data.selectedDay = this.numberSurface;
-      this._eventOutput.emit('click', data);
+      this._eventOutput.emit('click', this);
     }.bind(this));
 
     this.add(this.backgroundModifier).add(this.backgroundSurface);
@@ -60,15 +91,14 @@ define(function(require, exports, module) {
   function _createNumberSurface() {
     this.numberSurface = new Surface({
       size: [34, true],
-      content: this.options.number || '',
+      content: this.options.day || '',
       properties: {
         lineHeight: '34px',
         textAlign: 'center',
-        color: this.options.fontColor,
-        fontWeight: 'thin',
-        fontFamily: 'sans-serif',
+        color: this.isWeekend() ? this.options.weekendColor : this.options.weekdayColor,
+        fontFamily: this.options.font,
         pointerEvents: 'none',
-        id: this.options.id
+        zIndex: 1
       }
     });
 
