62e0b6afb1414c2ddc8bf339f3d5c09491c2273b
Alex West <alexanderwwest@gmail.com>
2014-06-24 15:36:05 -0700

    Refactored Utilities import to work without instantiation

--- app/src/utilities.js
+++ app/src/utilities.js
@@ -2,45 +2,46 @@
 
 define(function(require, exports, module){
 
-	function Utilities(){
-		this.saveEvent = function saveEvent(newEvent) {
-		  var eventDate = newEvent.date;
-		  console.log(eventDate);
+	var Utilities = {};
+	Utilities.saveEvent = function saveEvent(newEvent) {
+	  var eventDate = newEvent.date;
+	  console.log(eventDate);
 
-		  //Retrieve events for date from local storage
-		  var events = window.localStorage.getItem(eventDate);
+	  //Retrieve events for date from local storage
+	  var events = window.localStorage.getItem(eventDate);
 
-		  //Check if any events exist
-		  if(events === null){
+	  //Check if any events exist
+	  if(events === null){
 
-		    //Initialize events to array
-		    events = [];
-		  }else{
+	    //Initialize events to array
+	    events = [];
+	  }else{
 
-		    //Parse events array from string
-		    events = JSON.parse(events);
-		  }
+	    //Parse events array from string
+	    events = JSON.parse(events);
+	  }
 
-		  //create new event object from input - TBD in Calling Function
+	  //create new event object from input - TBD in Calling Function
 
-		  //Object schema:
-		  //{date: eventToBeSaved.date, 
-		  // start: eventToBeSaved.start, 
-		  // end: eventEndTime, 
-		  // title: eventTitle, 
-		  // description: eventBody};
-		  
-		  //add new event to events array
-		  events.push(newEvent);
-		  //Store updated events array in local storage
-		  window.localStorage.setItem(eventDate, JSON.stringify(events));
-		};
+	  //Object schema:
+	  //{date: eventToBeSaved.date, 
+	  // start: eventToBeSaved.start, 
+	  // end: eventEndTime, 
+	  // title: eventTitle, 
+	  // description: eventBody};
+	  
+	  //add new event to events array
+	  events.push(newEvent);
 
-		this.getEvents = function getEvents(date){
-		  //returns events array for given date
-		  return JSON.parse(window.localStorage.getItem(date));
-		};
-	}
+	  //Store updated events array in local storage
+	  window.localStorage.setItem(eventDate, JSON.stringify(events));
+	};
+
+	Utilities.getEvents = function getEvents(date){
+
+	  //returns events array for given date
+	  return JSON.parse(window.localStorage.getItem(date));
+	};
 
 	module.exports = Utilities;
 });
