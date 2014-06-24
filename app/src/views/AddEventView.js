d36d2340645068c9ae48671c4ef66d253a1bb3e0
Alex West <alexanderwwest@gmail.com>
2014-06-24 15:07:42 -0700

    Implemented HTML5 local storage

--- app/src/views/AddEventView.js
+++ app/src/views/AddEventView.js
@@ -12,6 +12,10 @@ define(function(require, exports, module) {
     var ScrollView = require('famous/views/Scrollview');
     var Modifier = require('famous/core/Modifier');
 
+
+    var hUnits = window.innerHeight / 500;
+    var wUnits = window.innerWidth / 500;
+
     // Constructor function for our SlideShowView class
     function AddEventView() {
 
@@ -33,35 +37,47 @@ define(function(require, exports, module) {
         this.background = new Surface({
             size: [undefined, undefined],
             properties: {
-                backgroundColor: 'white'
+                backgroundColor: '#FAFAFA'
             }
         });
         var backgroundMod = new Modifier({
-            transform: Transform.behind
+            transform: Transform.behind,
         });
         this.add(backgroundMod).add(this.background)
         //Focus Title Field
 
         //Access app header??? But not this:
         this.header = new Surface({
-            content:'Add Event'
+            content:'Add Event',
+            size: [true, undefined],
+            properties: {
+                textAlign: 'center',
+                fontFamily: 'sans-serif',
+                borderBottom: '1px solid lightgrey'
+            }
         });
+        var headerMod = new Modifier({
+            origin : [0.5, 0.5],
+            align: [0.5, 0.5]
+        })
 
         //TBD: Add Cancel and Done buttons on Header
 
-        this.add(this.header);
+        this.add(headerMod).add(this.header);
 
         this.titleField = new InputSurface({
             placeholder: 'Title',
             type: 'text',
             properties: {
                 // backgroundColor: 'red'
+                fontFamily: 'sans-serif',
+                borderBottom: '1px solid lightgrey'
             },
-            size: [100, true]
+            size: [undefined, true]
         });
         var titleFieldModifier = new Modifier({
             origin: [0.5, 0.5],
-            align: [0.1, 0.1]
+            align: [0.5, 0.1]
         });
         this.add(titleFieldModifier).add(this.titleField);
 
@@ -71,62 +87,80 @@ define(function(require, exports, module) {
         this.locationField = new InputSurface({
             placeholder: 'Location',
             type: 'text',
-            size: [100, true],
+            size: [undefined, true],
+            properies: {
+                fontFamily: 'sans-serif',
+                borderBottom: '1px solid lightgrey'
+            }
         });
         var locationModifier = new Modifier({
-            align: [0.1, 0.1],
-            transform: Transform.translate(0, 50, 0)
+            align: [0.5, 0.1],
+            transform: Transform.translate(0, 50 * hUnits, 0)
         });
         this.add(locationModifier).add(this.locationField);
 
         this.dateField = new InputSurface({
             placeholder: 'Date',
             type: 'date',
-            size: [100, true],
+            size: [undefined, true],
+            properties: {
+                fontFamily: 'sans-serif',
+                borderBottom: '1px solid lightgrey'
+            }
         });
         var dateModifier = new Modifier({
-            align: [0.1, 0.1],
-            transform: Transform.translate(0, 75, 0)
+            align: [0.5, 0.1],
+            transform: Transform.translate(0, 75 * hUnits, 0)
         });
         this.add(dateModifier).add(this.dateField);
 
         this.startField = new InputSurface({
-            // placeholder: 'Starts',
             type: 'time',
-            size: [100, true],
+            size: [undefined, true],
+            properties: {
+                fontFamily: 'sans-serif',
+                borderBottom: '1px solid lightgrey'
+            }
         });
         var startFieldModifier = new Modifier({
-            align: [0.1, 0.1],
-            transform: Transform.translate(0, 125, 0)
+            align: [0.5, 0.1],
+            transform: Transform.translate(0, 125 * hUnits, 0)
         })
         this.add(startFieldModifier).add(this.startField);
 
         this.endField = new InputSurface({
             placeholder: 'Ends',
             type: 'time',
-            size: [100, true],
+            size: [undefined, true],
             properties: {
-                backgroundColor: 'white'
+                backgroundColor: 'white',
+                fontFamily: 'sans-serif',
+                borderBottom: '1px solid lightgrey'
             }
         });
         var endFieldModifier = new Modifier({
-            transform: Transform.translate(0, 150, 0),
-            align: [0.1, 0.1]
+            transform: Transform.translate(0, 150 * hUnits, 0),
+            align: [0.5, 0.1]
         })
         this.add(endFieldModifier).add(this.endField);
 
         this.repeatField = new Surface({
             content: 'Repeat     Never >',
-            size: [100, true],
+            size: [undefined, true],
             properties: {
-                backgroundColor: 'white'
+                backgroundColor: 'white',
+                fontFamily: 'sans-serif',
+                borderBottom: '1px solid lightgrey'
             }
             //Is there a true/false input type?
             //Or implement as link to new view  like in iOS cal
         });
         var repeatFieldModifier = new Modifier({
-            transform: Transform.translate(0, 200, 0),
-            align: [0.1, 0.1]
+            transform: Transform.translate(0, 200 * hUnits, 0),
+            align: [0.5, 0.1],
+            properties: {
+                fontFamily: 'sans-serif'
+            }
         });
         this.add(repeatFieldModifier).add(this.repeatField);
 
@@ -134,6 +168,26 @@ define(function(require, exports, module) {
         //newEvent = createEvent.call(this);
 
         //saveEvent(newEvent);
+
+        this.saveButton = new InputSurface({
+            type: 'button',
+            size: [undefined, true],
+            value: 'Save',
+            properties: {
+                backgroundColor: 'white',
+                fontFamily: 'sans-serif'
+            }
+        });
+        this.saveButton.on('click', function(){
+            saveEvent(createEvent.call(this));
+        }.bind(this));
+
+
+        var saveModifier = new Modifier({
+            transform: Transform.translate(0, 250 * hUnits, 0),
+            align: [0.5, 0.1]
+        })
+        this.add(saveModifier).add(this.saveButton);
 
     }
 
@@ -148,15 +202,18 @@ define(function(require, exports, module) {
     function createEvent(){
         var eventData = {};
         eventData.title = this.titleField.getValue();
-        eventData.location = this.titleField.getValue();
+        eventData.date = this.dateField.getValue();
+        eventData.location = this.locationField.getValue();
         eventData.start = this.startField.getValue();
         eventData.end = this.endField.getValue();
-        eventData.repeat = this.repeatField.getValue();
+        // eventData.repeat = this.repeatField.getValue();
 
         return eventData;
     }
 
     function saveEvent(newEvent) {
+      var eventDate = newEvent.date;
+      console.log(eventDate);
 
       //Retrieve events for date from local storage
       var events = window.localStorage.getItem(eventDate);
