ab6e23bc328c3b431ca3eebfbf4c20db7f40fbf7
mikefreeman <michael.p.freeman@gmail.com>
2014-06-24 10:55:39 -0700

    Added date constants file

---
+++ app/src/config/DateConstants.js
@@ -0,0 +1,30 @@
+define(function(require, exports, module) {
+  var data = {};
+
+  data.monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+  data.daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
+
+  module.exports = data;
+});
