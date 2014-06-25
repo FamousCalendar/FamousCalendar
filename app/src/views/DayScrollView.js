5105d6f68f3bcbd07a740cb1b2aab90d8ad37c5e
Wayland Woodruff <waylandwoodruff@gmail.com>
2014-06-25 14:38:08 -0700

    InfiniteScrollView Implementation

--- app/src/views/DayScrollView.js
+++ app/src/views/DayScrollView.js
@@ -1,14 +1,21 @@
-/*** DayScrollView.js ***/
+/*** DayScrollview.js ***
+ *  Wayland Woodruff
+ *  WaylandWoodruff@gmail.com
+ */
 
 define(function(require, exports, module) {
-  var ViewSequence  = require('famous/core/ViewSequence');
-  var ScrollView    = require('famous/views/Scrollview');
+  var ViewSequence        = require('famous/core/ViewSequence');
+  var Scrollview          = require('famous/views/Scrollview');
+  var InfiniteScrollview  = require('views/InfiniteScrollview');
   
-  var DayView = require('views/DayView');
+  var AppSettings = require('config/AppSettings');
+  var DayView     = require('views/DayView');
   
-  function DayScrollView() {
-    ScrollView.apply(this, arguments);
-    this.setOptions(DayScrollView.DEFAULT_OPTIONS);
+  function DayScrollview() {
+    InfiniteScrollview.apply(this, arguments);
+    this.setOptions(DayScrollview.DEFAULT_OPTIONS);
+    
+    _setEventsUpdater.call(this);
     
     this.dayViews = [];
     this.viewSequence = new ViewSequence({
@@ -20,20 +27,55 @@ define(function(require, exports, module) {
     _createDayViews.call(this);
   }
   
-  DayScrollView.DEFAULT_OPTIONS = {
-    maxDayViews: 4
+  DayScrollview.DEFAULT_OPTIONS = {
+    maxDayViews: 7,
+    startTime: AppSettings.dayView.getStartTime()  //  time of day [hours, minutes] to be default start time
   };
   
-  DayScrollView.prototype = Object.create(ScrollView.prototype);
-  DayScrollView.prototype.constructor = DayScrollView;
+  DayScrollview.prototype = Object.create(InfiniteScrollview.prototype);
+  DayScrollview.prototype.constructor = DayScrollview;
+  
+  DayScrollview.prototype.setToStartPosition = function setToStartPosition(date, scrollToDate) {
+    //  Called when a day is selected from the month view
+    scrollToDate = (scrollToDate !== undefined) ? scrollToDate : false;
+    if (scrollToDate) {
+      //  scroll to date supplied, end scroll at default time for that date
+    } else if (date) {    //  loads events for the supplied date to the current DayView.
+    //  Determine the current DayView
+    //    Load events for 'date' on this DayView
+    //    Load events for adjacent dates on adjacent DayViews
+    //    
+    //  Then set scroller to default position on current DayView
+    }
+    this.setPosition(((this.dayViews[0].getSize())[1] * _timeToPositionPercentage(this.options.startPosition)));
+  }
   
   function _createDayViews() {
     for (var views = 0; views < this.options.maxDayViews; views++) {
       this.dayViews.push(new DayView({
-        scrollView: this
+        Scrollview: this
       }));
+      
+      if (views < 4) {
+        this.dayViews[views].buildEvents(views);
+      } else {
+        this.dayViews[views].buildEvents(views - 7);
+      }
     }
   }
   
-  module.exports = DayScrollView;
+  function _setEventsUpdater() {
+    this.updateNodeBuffer = function updateNodeBuffer(target, current, offset) {
+      target.buildEvents(current._date + offset);
+    };
+  } //  End _setEventsUpdater
+  
+  function _timeToPositionPercentage(time) {
+    //  Takes time as array: [hour, minutes] (military time)
+    return (time instanceof Array && time.length >= 2)
+      ? (((time[0] * 60) + time[1]) / 1440)
+      : null;
+  } //  End _timeToPositionPercentage
+  
+  module.exports = DayScrollview;
 });
--- app/src/views/DayView.js
+++ app/src/views/DayView.js
@@ -1,23 +1,31 @@
-/*** DayView.js ***/
-
+/*** DayView.js ***
+ *  Wayland Woodruff
+ *  WaylandWoodruff@gmail.com
+ */
+ 
 define(function(require, exports, module) {
   //  Dependencies
-  var View  = require('famous/core/View');
+  var RenderNode    = require('famous/core/RenderNode');
+  var Surface       = require('famous/core/Surface');
+  var View          = require('famous/core/View');
+  var StateModifier = require('famous/modifiers/StateModifier');
   
-  var RowsView      = require('views/RowsView');
-  var TimelineView  = require('views/TimelineView');
   var AppSettings   = require('config/AppSettings');
+  var TimelineView  = require('views/TimelineView');
   
   function DayView() {
     View.apply(this, arguments);
     
-    _createRows.call(this);
+    this._eventsNode  = new RenderNode();
+    this._date        = null;
+    
+    _createBackground.call(this);
     _createTimeline.call(this);
-    this.loadEvents();
   }
   
   DayView.DEFAULT_OPTIONS = {
-    scrollView: null
+    scrollView: null,
+    startTime: AppSettings.dayView.getStartTime()
   };
   
   //  Prototype Functions
@@ -26,24 +34,77 @@ define(function(require, exports, module) {
   
   DayView.prototype.getSize = function getSize() {
     //  This function is called by scrollview to determine spacing of each element in the scroll's collection
-    return this.rows.getTotalSize();
+    return (this.timeline)
+      ? [true, this.timeline.getSize()[1], 0]
+      : [true, 1440, 0];
   };  //  End DayView.prototype.getSize
   
-  DayView.prototype.loadEvents = function loadEvents() {
+  DayView.prototype.buildEvents = function buildEvents(date) {
     //  Instantiates the collection of event surfaces and their modifiers for a given day.
     //  Called each time the ScrollView cycles a DayView from one end of the collection to the other
+    this._date = date;
+    console.log('Updated value to:', this._date);
+    //  Clear all events on this dayView
+    //  Collect list of events for the current date from data storage
+    //  Build list of EventViews with corresponding modifiers
+    //  add modifier+eventview to this.eventsNode
   };  //  End DayView.prototype.loadEvents
   
   //  Helper Functions
-  function _createRows() {
-    this.rows = new RowsView({ scrollView: this.options.scrollView });
-    this.add(this.rows);
-  };  //  End _createRows
+  function _createBackground() {
+    var bgModifier = new StateModifier({
+      origin: [0, 0],
+      align: [0, 0],
+      //size: this.getSize()
+      size: [undefined, 1440]
+    });
+    
+    var bgSurface = new Surface({
+      properties: {
+        backgroundColor: '#BBBBAA',
+        zIndex: -10
+      }
+    });
+    
+    bgSurface.pipe(this.options.Scrollview);
+    this.add(bgModifier).add(bgSurface);
+  } //  End _createBackground
   
   function _createTimeline() {
     this.timeline = new TimelineView();
     this.add(this.timeline);
-  };  //  End _createTimeline
+  } //  End _createTimeline
   
   module.exports = DayView;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
