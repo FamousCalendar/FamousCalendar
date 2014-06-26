de10ae1654395526ea684befb8d1390ae3f0473e
Wayland Woodruff <waylandwoodruff@gmail.com>
2014-06-26 04:54:49 -0700

    Display Events on DayView

--- app/src/views/DayScrollView.js
+++ app/src/views/DayScrollView.js
@@ -7,6 +7,7 @@ define(function(require, exports, module) {
   var ViewSequence        = require('famous/core/ViewSequence');
   var Scrollview          = require('famous/views/Scrollview');
   var InfiniteScrollview  = require('views/InfiniteScrollview');
+  var Utilities           = require('utilities');
   
   var AppSettings = require('config/AppSettings');
   var DayView     = require('views/DayView');
@@ -25,29 +26,28 @@ define(function(require, exports, module) {
     this.sequenceFrom(this.viewSequence);
     
     _createDayViews.call(this);
+    this.setToDate(this.options.startDate);
   }
   
   DayScrollview.DEFAULT_OPTIONS = {
     maxDayViews: 7,
+    startDate: '2014-06-25',
     startTime: AppSettings.dayView.getStartTime()  //  time of day [hours, minutes] to be default start time
   };
   
   DayScrollview.prototype = Object.create(InfiniteScrollview.prototype);
   DayScrollview.prototype.constructor = DayScrollview;
   
-  DayScrollview.prototype.setToStartPosition = function setToStartPosition(date, scrollToDate) {
+  DayScrollview.prototype.setToDate = function setToDate(date, scrollToDate) {
     //  Called when a day is selected from the month view
     scrollToDate = (scrollToDate !== undefined) ? scrollToDate : false;
-    if (scrollToDate) {
-      //  scroll to date supplied, end scroll at default time for that date
-    } else if (date) {    //  loads events for the supplied date to the current DayView.
-    //  Determine the current DayView
-    //    Load events for 'date' on this DayView
-    //    Load events for adjacent dates on adjacent DayViews
-    //    
-    //  Then set scroller to default position on current DayView
+    
+    if (!scrollToDate) {
+      _resetDayViews.call(this, date);
+      this.setPosition(1440 * _timeToPositionPercentage(this.options.startTime));
+    } else {
+      var currentIndex = this._node.getIndex();
     }
-    this.setPosition(((this.dayViews[0].getSize())[1] * _timeToPositionPercentage(this.options.startPosition)));
   }
   
   function _createDayViews() {
@@ -55,23 +55,119 @@ define(function(require, exports, module) {
       this.dayViews.push(new DayView({
         Scrollview: this
       }));
-      
-      if (views < 4) {
-        this.dayViews[views].buildEvents(views);
-      } else {
-        this.dayViews[views].buildEvents(views - 7);
-      }
     }
-  }
+  } //  End _createDayViews
+  
+  function _dateStrToArr(dateStr) {
+    if (!dateStr) return;
+    
+    var result = [];
+    result.push(+(dateStr.slice(0, 4)));
+    result.push(+(dateStr.slice(5, 7)));
+    result.push(+(dateStr.slice(8)));
+    return result;
+  } //  End _dateStrToArr
+  
+  function _dateArrToStr(dateArr) {
+    if (!dateArr || !(dateArr instanceof Array)) return;
+    
+    var result = '';
+    if (dateArr[0] > 999) result += dateArr[0] + '-';
+    else if (dateArr[0] > 99) result += '0' + dateArr[0] + '-';
+    else if (dateArr[0] > 9) result += '00' + dateArr[0] + '-';
+    else if (dateArr[0] > -1) result += '000' + dateArr[0] + '-';
+    else result += '0000' + '-';
+    
+    result += (dateArr[1] > 9) ? dateArr[1] + '-' : '0' + dateArr[1] + '-';
+    result += (dateArr[2] > 9) ? dateArr[2] : '0' + dateArr[2];
+    return result;
+  } //  End _dateArrToStr
+  
+  function _resetDayViews(date) {
+    var currentIndex  = this._node.getIndex();
+    var array         = this.dayViews;
+    this.dayViews[currentIndex].setDate(date);
+    
+    for (var i = 0; i < array.length; i++) {
+      var targetIndex = ((currentIndex + i) < array.length) ? (currentIndex + i) : (currentIndex + i - array.length);
+      var dateOffset  = (i < (Math.floor((array.length - 0.5) / 2) + 1)) ? i : (i - array.length);
+      this.updateNodeBuffer(this.dayViews[targetIndex], this.dayViews[currentIndex], dateOffset);
+    }
+  } //  End _resetDayViews
   
   function _setEventsUpdater() {
     this.updateNodeBuffer = function updateNodeBuffer(target, current, offset) {
-      target.buildEvents(current._date + offset);
+      var currentDate = current.getDate();
+      currentDate = _dateStrToArr(currentDate);
+      var targetDate = _offsetDate(currentDate, offset);  //  targetDate will be an array
+      targetDate = _dateArrToStr(targetDate);
+      target.setDate(targetDate);
+      target.buildEvents(Utilities.getEvents(targetDate));
     };
   } //  End _setEventsUpdater
   
+  function _offsetDate(date, offset) {
+    if (!date) return null;
+    
+    var _31DayMonths = 5546;
+    var offsetDate = [date[0], date[1], (date[2] + offset)];
+    
+    function isLeapYear(year) {
+      return ((year % 400 === 0) || ((year % 100 !== 0) && (year % 4 === 0)));
+    }
+    
+    function nextMonth() {
+      var date = offsetDate.slice();
+      date[1] = ((date[1] + 1) < 13) ? (date[1] + 1) : 1;
+      if (date[1] === 1) date[0] += 1;
+      date[2] = 1;
+      
+      return date;
+    }
+    
+    function prevMonth() {
+      var date = offsetDate.slice();
+      date[1] = ((date[1] - 1) > 0) ? (date[1] - 1) : 12;
+      if (date[1] === 12) date[0] -= 1;
+      
+      if ((1 << date[1]) & _31DayMonths) date[2] = 31;
+      else if (date[1] === 2) date[2] = (isLeapYear(date[0])) ? 29 : 28;
+      else date[2] = 30;
+      
+      return date;
+    }
+    
+    if ((1 << date[1]) & _31DayMonths) {
+      if (offsetDate[2] < 1) {
+        return _offsetDate(prevMonth(), offsetDate[2]);
+      } else if (offsetDate[2] > 31) {
+        return _offsetDate(nextMonth(), (offsetDate[2] - 32));
+      } else {
+        return offsetDate;
+      }
+    } else if (date[1] === 2) {
+      if (offsetDate[2] < 1) {
+        return _offsetDate(prevMonth(), offsetDate[2]);
+      } else {
+        var maxDay = (isLeapYear(offsetDate[0])) ? 29 : 28;
+        if (offsetDate[2] > maxDay) return _offsetDate(nextMonth(), (offsetDate[2] - maxDay - 1));
+        else return offsetDate;
+      }
+    } else {
+      if (offsetDate[2] < 1) {
+        return _offsetDate(prevMonth(), offsetDate[2]);
+      } else if (offsetDate[2] > 30) {
+        return _offsetDate(nextMonth(), (offsetDate[2] - 31));
+      } else {
+        return offsetDate;
+      }
+    }
+  } //  End _offsetDate
+  
   function _timeToPositionPercentage(time) {
     //  Takes time as array: [hour, minutes] (military time)
+    if (!time || !(time instanceof Array)) return null;
+    
     return (time instanceof Array && time.length >= 2)
       ? (((time[0] * 60) + time[1]) / 1440)
       : null;
@@ -79,3 +175,24 @@ define(function(require, exports, module) {
   
   module.exports = DayScrollview;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
