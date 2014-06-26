2df6274ef25955749066e3c58503b90408a1b6dc
Alex West <alexanderwwest@gmail.com>
2014-06-25 17:21:53 -0700

    Added hasEvents utility, refactored calendar repeat to instantiate properties outside utility functions, fixed bugs

--- app/src/utilities.js
+++ app/src/utilities.js
@@ -4,7 +4,12 @@ define(function(require, exports, module){
 
 
 	 //Retrieve _calendar for date from local storage
-	var _calendar = JSON.parse(window.localStorage.getItem('calendar'))|| {};
+	var _calendar = JSON.parse(window.localStorage.getItem('calendar')) || {};
+	_calendar.repeat = _calendar.repeat || {};
+	_calendar.repeat['daily'] = _calendar.repeat['daily'] || [];
+	_calendar.repeat['weekly'] = _calendar.repeat['weekly'] || [];
+	_calendar.repeat['monthly'] = _calendar.repeat['monthly'] || [];
+	_calendar.repeat['yearly'] = _calendar.repeat['yearly'] || {};
 
 	var Utilities = {};
 
@@ -39,25 +44,25 @@ define(function(require, exports, module){
 
 	  if(recurrence !== 'false'){
 	  	if(recurrence === 'daily'){
-	  		_calendar.repeat['daily'] = _calendar.repeat['daily'] || [];
+	  		
 	  		_calendar.repeat['daily'].push(newEvent);
 	  	}else if(recurrence === 'weekly'){
-	  		_calendar.repeat['weekly'] = _calendar.repeat['weekly'] || {};
+	  		
 	  		_calendar.repeat['weekly'][dateObj.getDay()] = _calendar.repeat['weekly'][dateObj.getDay()] || [];
 	  		_calendar.repeat['weekly'][dateObj.getDay()].push(newEvent);
 	  	}else if(recurrence === 'monthly'){
+	  		
 	  		_calendar.repeat[recurrence][eventDate.slice(-2)] = _calendar.repeat.recurrence[eventDate.slice(-2)] || [];
 	  		_calendar.repeat[recurrence][eventDate.slice(-2)].push(newEvent);
 	  	}else if(recurrence === 'yearly'){
 	  		_calendar.repeat[recurrence][eventDate.slice(5)] = _calendar.repeat.recurrence[eventDate.slice(5)] || [];
 	  		_calendar.repeat[recurrence][eventDate.slice(5)].push(newEvent);
 	  	}
-	  	_calendar.repeat[recurrence][day] = newEvent;
 	  }
 
-	  _calendar.eventDate = _calendar.eventDate || [];
+	  _calendar[eventDate] = _calendar[eventDate] || [];
 	  //add new event to _calendar array
-	  _calendar.eventDate.push(newEvent);
+	  _calendar[eventDate].push(newEvent);
 
 	  //Store updated _calendar array in local storage
 	  window.localStorage.setItem('calendar', JSON.stringify(_calendar));
@@ -67,15 +72,26 @@ define(function(require, exports, module){
 		return _calendar;
 	}
 
+	Utilities.hasEvents = function hasEvents(date){
+		var dailyEvents = _calendar.repeat.daily || [];
+		var weeklyEvents = _calendar.repeat.weekly[new Date(date).getDay()] || [];
+		var monthlyEvents = _calendar.repeat.monthly[date.slice(-2)] || [];
+		var yearlyEvents = _calendar.repeat.yearly[date.slice(5)] || [];
+		if(_calendar[date].concat(dailyEvents, weeklyEvents, monthlyEvents, yearlyEvents) !== undefined){
+			return true;
+		}
+		return false;
+	}
+
 	Utilities.getEvents = function getEvents(date){
       var result = [];
       var dailyEvents = _calendar.repeat.daily || [];
       var weeklyEvents = _calendar.repeat.weekly[new Date(date).getDay()] || [];
       var monthlyEvents = _calendar.repeat.monthly[date.slice(-2)] || [];
       var yearlyEvents = _calendar.repeat.yearly[date.slice(5)] || [];
-
+      console.log(_calendar);
 	  //returns _calendar array for given date
-	  return result.concat(_calendar.date, dailyEvents, weeklyEvents, monthlyEvents, yearlyEvents);
+	  return result.concat(_calendar[date], dailyEvents, weeklyEvents, monthlyEvents, yearlyEvents);
 
 	};
 
