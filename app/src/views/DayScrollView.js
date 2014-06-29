0da2a70eb412d2048c2f33d62ed4060e20672424
Wayland Woodruff <waylandwoodruff@gmail.com>
2014-06-29 14:28:41 -0700

    Parial autoscroll implemented; still glitchy

--- app/src/views/DayScrollView.js
+++ app/src/views/DayScrollView.js
@@ -4,14 +4,17 @@
  */
 
 define(function(require, exports, module) {
-  var ViewSequence        = require('famous/core/ViewSequence');
-  var Scrollview          = require('famous/views/Scrollview');
-  var InfiniteScrollview  = require('views/InfiniteScrollview');
-  var Utilities           = require('utilities');
+  var Transform       = require('famous/core/Transform');
+  var ViewSequence    = require('famous/core/ViewSequence');
+  var Easing          = require('famous/transitions/Easing');
+  var Transitionable  = require('famous/transitions/Transitionable');
+  var Scrollview      = require('famous/views/Scrollview');
   
-  var AppSettings = require('config/AppSettings');
-  var TimeUtil    = require('util/TimeUtil');
-  var DayView     = require('views/DayView');
+  var AppSettings         = require('config/AppSettings');
+  var Utilities           = require('utilities');
+  var TimeUtil            = require('util/TimeUtil');
+  var DayView             = require('views/DayView');
+  var InfiniteScrollview  = require('views/InfiniteScrollview');
   
   function DayScrollview() {
     InfiniteScrollview.apply(this, arguments);
@@ -30,7 +33,19 @@ define(function(require, exports, module) {
     _createDayViews.call(this);
     this.setToDate(this.options.startDate);
     
+    window.ascroll = function(val) {
+      this.setToDate( val, true );
+    }.bind(this);
+    
+    window.gettime = function() {
+      console.log('Position:', this.getPosition());
+      console.log('Day:', this.dayViews[this._node.getIndex()].getDate());
+    }.bind(this);
+    
   }
+  
+  /** @const */
+  var TOLERANCE = 0.5;
   
   DayScrollview.DEFAULT_OPTIONS = {
     maxDayViews: 7,
@@ -38,11 +53,17 @@ define(function(require, exports, module) {
     startTime: AppSettings.dayView.getStartTime()  //  time of day [hours, minutes] to be default start time
   };
   
+  /**@positionFrom() Transforms**/
+  var POSITION_TRANSFORM = {
+    'default': _default,
+    'autoscroll': _autoScroller
+  };
+  
   DayScrollview.prototype = Object.create(InfiniteScrollview.prototype);
   DayScrollview.prototype.constructor = DayScrollview;
   
-  /**
-   * @method setToDate
+  /**@method setToDate
+   * 
    * Forces the currently displayed DayView to change to the date specified.
    *
    * @param {string} date : A hyphen-delimited date in the format of "yyyy-mm-dd". This is the date
@@ -52,73 +73,85 @@ define(function(require, exports, module) {
    *                                 Set this to true only if the DayView is visible when updated.
    */
   DayScrollview.prototype.setToDate = function setToDate(date, scrollToDate) {
-    //  Called when a day is selected from the month view
-    
     scrollToDate = (scrollToDate !== undefined) ? scrollToDate : false;
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-      _resetDayViews.call(this, date);
-      this.setPosition(1440 * TimeUtil.timeToPositionPercentage(this.options.startTime));
-    ///////////////////////////////////////////////////////////////////////////////////////////////
     
-    /*
     if (!scrollToDate) {
       _resetDayViews.call(this, date);
       this.setPosition(1440 * TimeUtil.timeToPositionPercentage(this.options.startTime));
     } else {
       var currentIndex = this._node.getIndex();
-      var now = TimeUtil.dateStrToArr(this.dayViews[currentIndex].getDate());
-      if (!now) return;
-      var then = TimeUtil.dateStrToArr(date);
-      if (!then) return;
+      var now = []; //  The current time
+      now.push(TimeUtil.dateStrToArr(this.dayViews[currentIndex].getDate()));
+      now.push(TimeUtil.calcTimeArr(this.getPosition()));
+      if (!now[0] || !now[1]) return;
+      var then = [];    //  The time we want to transition to
+      then.push(TimeUtil.dateStrToArr(date));
+      then.push(this.options.startTime);
+      if (!then[0] || !then[1]) return;
       
-      //debugger;
-      this._autoscroll.minDiff = _timeDiffMin.call(this, now, then); //  Negative value means target time is before current time
-      
-      //  Set new getPosition function with this._scroller.positionFrom()
-      this._scroller.positionFrom(this.getAutoscrollPosition.bind(this));
-      //compare target date to current date
-      //  Calculate difference in time between target date at start position ([8,0]) and current position on current date
-      //  transform scrollview particle by this difference
+      this._autoscroll.minDiff = TimeUtil.timeToPixels(then[1]) + TimeUtil.timeDiffMin.call(this, then[0], now[0], then[1], now[1]); //  Negative value means target time is before current time
+      var transitioner = new Transitionable(this.getPosition());
+      this._scroller.positionFrom(transitioner);
+      transitioner.set(this._autoscroll.minDiff, {duration: 750, curve: Easing.outCubic}, function() {
+        this._autoscroll.minDiff = 0;
+        this.setPosition(transitioner.get());
+        this._scroller.positionFrom(this.getPosition.bind(this));
+        console.log('It is currently:', this.dayViews[this._node.getIndex()].getDate());
+      }.bind(this));
     }
-    */
   }
   
+    /** @enum */
+    var SpringStates = {
+        NONE: 0,
+        EDGE: 1,
+        PAGE: 2
+    };
+
+    function _shiftOrigin(amount) {
+        this._edgeSpringPosition += amount;
+        this._pageSpringPosition += amount;
+        this.setPosition(this.getPosition() + amount);
+        if (this._springState === SpringStates.EDGE) {
+            this.spring.setOptions({anchor: [this._edgeSpringPosition, 0, 0]});
+        }
+        else if (this._springState === SpringStates.PAGE) {
+            this.spring.setOptions({anchor: [this._pageSpringPosition, 0, 0]});
+        }
+    }
+
+
+  
   /**
-   * @method getAutoscrollPosition
+   */
+  function _setScrollTransform(scrollTrans) {
+    scrollTrans = POSITION_TRANSFORM[scrollTrans].bind(this);
+    var positionSetter = scrollTrans();
+    this._scroller.positionFrom(positionSetter);
+  } //  End _setScrollTransform
+  
+  /**
+   * @method _autoScroll
    * Used to autoscroll the DayViews when setToDate is invoked with true passed in as a second argument.
    * This function is passed to DayScrollview._scroller.positionFrom() to make the scroll happen. After
    * the scroll completes, the default getter function should be reset using positionFrom.
    *
    * @return {number} The position to scroll to
    */
-  DayScrollview.prototype.getAutoscrollPosition = function getAutoscrollPosition() {
-    var distance  = this._autoscroll.minDiff;
-    
-    if (distance < 0) {
-      if (distance < -100) {
-        this._autoscroll.minDiff += 100;
-        return this.getPosition() - 100;
-      } else {
-        this._autoscroll.minDiff = 0;
-        this._scroller.positionFrom(this.getPosition.bind(this));
-        return this.getPosition() - distance;
-      }
-    } else {
-      if (distance > 100) {
-        this._autoscroll.minDiff -= 100;
-        return this.getPosition() + 100;
-      } else {
-        this._autoscroll.minDiff = 0;
-        this._scroller.positionFrom(this.getPosition.bind(this));
-        return this.getPosition() + distance;
-      }
-    }
-    
-    //return this._particle.getPosition1D();
-  };  //  End DayScrollview.prototype.getAutoscrollPosition
+  function _autoScroller() {
+    //return (this.options.direction === Utility.Direction.X) ? Transform.translate(offset, 0) : Transform.translate(0, offset);
+    return this._transmogrifier;
+  } //  End _autoScroller
+  
   
   /**
-   * @method _createDayViews
+   */
+  function _default(offset) {
+    return (this.options.direction === Utility.Direction.X) ? Transform.translate(offset, 0) : Transform.translate(0, offset);
+  };
+  
+  /**@method _createDayViews
+   * 
    * Generates blank DayViews and pushes them to this.dayViews (7 DayViews by default). The DayScrollview
    * instance is passed as an option to the DayView so that nested surfaces can pipe events to it when they
    * are instantiated.
