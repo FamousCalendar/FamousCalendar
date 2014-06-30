6347dee241785aa00e85f9767ac56d852ad5cb1f
Alex West <alexanderwwest@gmail.com>
2014-06-30 13:01:10 -0700

    Implemented event editing

--- app/src/utilities.js
+++ app/src/utilities.js
@@ -5,6 +5,7 @@ define(function(require, exports, module){
 	 //Retrieve _calendar for date from local storage
 	var _calendar = JSON.parse(window.localStorage.getItem('calendar')) || {};
 	_calendar.repeat = _calendar.repeat || {};
+	_calendar.currentID = _calendar.currentID || 0;
 	_calendar.repeat['daily'] = _calendar.repeat['daily'] || [];
 	_calendar.repeat['weekly'] = _calendar.repeat['weekly'] || {};
 	_calendar.repeat['monthly'] = _calendar.repeat['monthly'] || {};
@@ -57,6 +58,8 @@ define(function(require, exports, module){
 
 	  _calendar[eventDate] = _calendar[eventDate] || [];
 	  //add new event to _calendar array
+
+	  newEvent.eventID = _calendar.currentID++;
 	  _calendar[eventDate].push(newEvent);
 
 	  //Store updated _calendar array in local storage
@@ -73,10 +76,8 @@ define(function(require, exports, module){
 		var weeklyEvents = _calendar.repeat.weekly[new Date(date).getDay()] || [];
 		var monthlyEvents = _calendar.repeat.monthly[date.slice(-2)] || [];
 		var yearlyEvents = _calendar.repeat.yearly[date.slice(5)] || [];
-		if(dateEvents.concat(dailyEvents, weeklyEvents, monthlyEvents, yearlyEvents).length > 0){
-			return true;
-		}
-		return false;
+
+		return (dateEvents.concat(dailyEvents, weeklyEvents, monthlyEvents, yearlyEvents).length > 0);
 	}
 
 	Utilities.getEvents = function getEvents(date){
@@ -90,6 +91,74 @@ define(function(require, exports, module){
 	  return result.concat(dateEvents, dailyEvents, weeklyEvents, monthlyEvents, yearlyEvents);
 
 	};
+
+	function _findEvent(event, callback){
+		if(event.repeat === 'false'){
+			var dateArray = _calendar[event.date];
+			for(var x = 0; x < dateArray.length; x++){
+				if(dateArray[x] === event){
+					callback(dateArray, dateArray[x], x);
+				}
+			}
+			// _calendar[event.date] = dateArray;
+		}else if(event.repeat === 'daily'){
+			var dailyArray = _calendar.repeat.daily;
+			for(var x = 0; x < dailyArray.length; x++){
+				if(dailyArray[x] === event){
+					callback(dailyArray, dailyArray[x], x);
+				}
+			}
+		}else if(event.repeat === 'weekly'){
+			var weeklyArray = _calendar.repeat.weekly[new Date(event.date).getDay()];
+			for(var x = 0; x < weeklyArray.length; x++){
+				if(weeklyArray[x] === event){
+					callback(weeklyArray, weeklyArray[x], x);
+				}
+			}
+		}else if(event.repeat === 'monthly'){
+			var monthlyArray = _calendar.repeat.monthly[event.date.slice(-2)];
+			for(var x = 0; x < monthlyArray.length; x++){
+				if(monthlyArray[x] === event){
+					callback(monthlyArray, monthlyArray[x], x);
+				}
+			}
+		}else if(event.repeat === 'yearly'){
+			var yearlyArray = _calendar.repeat.yearly[event.date.slice(5)];
+			for(var x = 0; x < yearlyArray.length; x++){
+				if(yearlyArray[x] === event){
+					callback(yearlyArray, yearlyArray[x], x);
+				}
+			}
+		}
+	}
+
+	Utilities.deleteEvent = function(event){
+		_findEvent(event, function(arr, item, index){
+			arr.splice(index, 1);
+		});
+		window.localStorage.setItem('calendar', JSON.stringify(_calendar));
+	};
+
+	Utilities.editEvent = function(oldEvent, updatedEvent){
+		_findEvent(oldEvent, function(arr, item, index){
+			arr[index] = updatedEvent;
+		});
+		window.localStorage.setItem('calendar', JSON.stringify(_calendar));
+	};
+
+	//Must .bind(this)
+	Utilities.createEvent = function _createEvent(){
+	    return { 
+	        title : this.titleField.getValue(),
+	        date : this.dateField.getValue(),
+	        location : this.locationField.getValue(),
+	        start : this.startField.getValue(),
+	        end : this.endField.getValue(),
+	        repeat : repeatValue.value
+	    };
+	}
+
+
 
 
 	module.exports = Utilities;
