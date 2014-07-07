28e901babf1138e9074da3e2f2e704feb938c941
mikefreeman <michael.p.freeman@gmail.com>
2014-07-07 09:11:50 -0700

    Added generateDateString

--- app/src/config/DateConstants.js
+++ app/src/config/DateConstants.js
@@ -26,5 +26,11 @@ define(function(require, exports, module) {
     'Saturday'
   ];
 
+  data.generateDateString = function(year, month, day) {
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+    return year + '-' + month + '-' + day;
+  }
+
   module.exports = data;
 });
