316543f18e5f020b05bde50c999bf71b994bc059
mikefreeman <michael.p.freeman@gmail.com>
2014-07-07 12:34:37 -0700

    Added functions for previous and next month and dates

--- app/src/config/DateConstants.js
+++ app/src/config/DateConstants.js
@@ -36,5 +36,70 @@ define(function(require, exports, module) {
     return this.daysOfWeek[weekDay] + ' ' + this.monthNames[month] + ' ' + day + ', ' + year;
   };
 
+  data.calcNextMonth = function(month, year) {
+    var nextMonth;
+    if (month === 11) {
+      nextMonth = { month: 0, year: year + 1 };
+    } else {
+      nextMonth = { month: month + 1, year: year };
+    }
+
+    return nextMonth;
+  };
+
+  data.calcPreviousMonth = function(month, year) {
+    var priorMonth;
+    if (month === 0) {
+      priorMonth = { month: 11, year: year - 1 };
+    } else {
+      priorMonth = { month: month - 1, year: year };
+    }
+
+    return priorMonth;
+  };
+
+  data.getPreviousDate = function(dateObj) {
+    var date = {};
+    var previousMonthYear;
+
+    if (dateObj.day === 1) {
+      previousMonthYear = this.calcPreviousMonth(dateObj.month, dateObj.year);
+      date.year = previousMonthYear.year;
+      date.month = previousMonthYear.month;
+      date.day = new Date(date.year, date.month + 1, 0).getDate();
+      date.weekDay = (dateObj.weekDay + 6) % 7;
+    } else {
+      date.year = dateObj.year;
+      date.month = dateObj.month;
+      date.day = dateObj.day - 1;
+      date.weekDay = (dateObj.weekDay + 6) % 7;
+    }
+
+    return date;
+  };
+
+  data.getNextDate = function(dateObj) {
+    var date = {};
+    var nextMonthYear;
+    var daysInMonth = new Date(dateObj.year, dateObj.month + 1, 0).getDate();
+
+    if (dateObj.day === daysInMonth) {
+      nextMonthYear = this.calcNextMonth(dateObj.month, dateObj.year);
+      date.year = nextMonthYear.year;
+      date.month = nextMonthYear.month;
+      date.day = 1;
+      date.week = 1;
+      date.weekDay = (dateObj.weekDay + 1) % 7;
+    } else {
+      date.year = dateObj.year;
+      date.month = dateObj.month;
+      date.day = dateObj.day + 1;
+      date.weekDay = (dateObj.weekDay + 1) % 7;
+    }
+
+    console.log(date);
+    return date;
+  };
+
   module.exports = data;
 });
